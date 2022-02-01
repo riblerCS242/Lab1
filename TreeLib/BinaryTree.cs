e014743b5a1caaac137893afeaad40c0ada1ad8b
Randy Ribler <ribler@lynchburg.edu>
2022-02-01 14:15:04 -0500

    Add project files.

---
+++ TreeLib/BinaryTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLib
+{
+
+    public class BinaryTree<KeyType, ValueType>
+           where KeyType : IComparable<KeyType>
+    {
+        Node<KeyType, ValueType> Root = null;
+
+        //public BinaryTree<KeyType, ValueType>()
+        //{
+        //    Root = null;
+        //}
+        //    |
+
+        public string printConnections()
+        {
+            StringBuilder builder = new StringBuilder();
+            if (Root != null)
+            {
+                //Root.printConnections(builder);
+            }
+            return builder.ToString();
+        }
+
+        public int Height()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            return builder.ToString();
+        }
+
+        public void Insert(KeyType key,
+        ValueType payload)
+        {
+        }
+
+        public IEnumerable<Tuple<KeyType, ValueType>> InOrderTraversal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAtRoot(KeyType key, ValueType payload)
+        {
+        }
+    }
+}
---
+++ TreeLib/Node.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeLib
+{
+    public class Node<KeyType, PayloadType> where
+        KeyType : IComparable<KeyType>
+    {
+        public Node<KeyType, PayloadType> LeftChild = null;
+        public Node<KeyType, PayloadType> RightChild = null;
+        public KeyType Key { get; set; }
+        public PayloadType Payload { get; set; }
+
+        public Node(KeyType key, PayloadType payload)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Tuple<KeyType, PayloadType>> InOrderTraversal(
+            Node<KeyType, PayloadType> root, List<Tuple<KeyType, PayloadType>> inOrderList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToString(StringBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void RotateRight(ref Node<KeyType,
+            PayloadType> root)
+        {
+        }
+
+        public static void RotateLeft(ref Node<KeyType, PayloadType> root)
+        {
+        }
+
+        public static void Insert(Node<KeyType, PayloadType> root,
+            KeyType key, PayloadType payload)
+        {
+        }
+
+        public static void InsertAtRoot(ref Node<KeyType, PayloadType> root,
+        KeyType key, PayloadType payload)
+        {
+        }
+
+        public int Height()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
